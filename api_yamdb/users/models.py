0ac50a5b3f1c2bed8ab9804105d3ce0a334edbdc
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2023-09-27 07:18:20 +0300

    Add CustomUser model

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOISES = [
+        ('USER', 'user'),
+        ('MODER', 'moderator'),
+        ('ADMIN', 'admin'),
+    ]
+    bio = models.TextField(verbose_name='Биография', blank=True)
+    role = models.CharField(verbose_name='Роль', default='USER')
