38be07773d19e3159878431be00ac140e89ced6b
Roman Matrosov <roman.matrosov.frontend@yandex.ru>
2023-10-05 03:02:52 +0300

    Change assert to if

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -198,12 +198,13 @@ class UserModelViewSet(viewsets.ModelViewSet):
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
         lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
-        assert lookup_url_kwarg in self.kwargs, (
-            'Expected view %s to be called with a URL keyword argument '
-            'named "%s". Fix your URL conf, or set the `.lookup_field` '
-            'attribute on the view correctly.' %
-            (self.__class__.__name__, lookup_url_kwarg)
-        )
+        if lookup_url_kwarg not in self.kwargs:
+            raise Exception(
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+            )
         filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
         obj = None
         username = filter_kwargs['pk']
